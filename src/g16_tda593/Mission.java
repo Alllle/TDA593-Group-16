7f41fceea6ada0047d934f821f4dde1ad4620d2e
Asmatkhan <jilor-man@hotmail.com>
2017-11-24 11:24:40 +0100

    implement robot class

---
+++ src/g16_tda593/Mission.java
@@ -0,0 +1,18 @@
+package g16_tda593;
+
+import project.Point;
+
+public class Mission {
+	
+	Point point;
+
+	public void addPoint(Point point){
+		this.point = point;
+	}
+	
+	public Point getPoint() {
+		// TODO Auto-generated method stub
+		return point;
+	}
+
+}
--- src/g16_tda593/Robot.java
+++ src/g16_tda593/Robot.java
@@ -3,7 +3,55 @@ import project.AbstractRobot;
 import project.Point;
 
 public class Robot extends AbstractRobot {
+	Point position;
+	String name;
+	Mission mission;
+	
 	public Robot(Point position, String name) {
 		super(position, name);
+		this.position = position;
+		this.name = name;
 	}
+	
+	public void executeMission(){
+		this.setDestination(this.mission.getPoint());
+	}
+	
+	public void assignMission(Mission mission){
+		this.mission = mission;
+	}
+	
+	
+	/**
+	 * strategy is not user in this assignment yet
+	public void changeStrategy(Strategy strategy){
+	}
+	*/
+	
+	/**
+	 * @return Point
+	 */
+	public Point getPosition(){
+		return this.position;
+	}
+	
+	/**
+	 * 
+	public void notify(){
+	}
+	*/
+	
+	/**
+	 *
+	public int getBatteryStatus(){
+		return 0;
+	}
+	*/
+	
+	/**
+	 *
+	 */
+	 public void stopEverything(){
+		 this.setDestination(this.position);
+	 }
 }
