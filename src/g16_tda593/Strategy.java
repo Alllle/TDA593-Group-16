08b9613fe9a7d53f3921f79ba9fb59add6dc5984
andreasmann <andreasmann94@gmail.com>
2017-12-08 10:32:20 +0100

    Added sequence diagram for assignment 5

---
+++ src/g16_tda593/Strategy.java
@@ -0,0 +1,23 @@
+package g16_tda593;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Strategy {
+
+	
+	public Strategy() {
+		
+	}
+	
+	public void run() {
+		try {
+			TimeUnit.SECONDS.sleep(2);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
